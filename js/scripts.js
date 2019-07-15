7eec676e07d123a9e37a9fac95418a7cdb5a1f56
Zarina Lavrenenko <zari.magura@gmail.com>
2019-07-15 17:49:21 +0300

    test2

--- js/scripts.js
+++ js/scripts.js
@@ -2,50 +2,85 @@
 $(document).ready(function () {
     $(".owl-carousel.brands").owlCarousel({
         loop: true,
-        items: 6
-    });
-});
-
-
-$(document).ready(function () {
-
-    $('.owl-carousel.brands').owlCarousel({
-        loop: true,
-        margin: 0,
-        nav: true,
+        items: 6,
         responsive: {
             0: {
                 items: 1
             },
+            400: {
+                items: 2
+            },
             600: {
-                items: 1
+                items: 3
+            },
+            850: {
+                items: 5
             },
             1000: {
-                items: 1
+                items: 6
             }
         }
     });
 });
 
+
+// $(document).ready(function () {
+
+    // $('.owl-carousel.brands').owlCarousel({
+    //     loop: true,
+    //     margin: 10,
+    //     nav: true,
+    //     responsive: {
+    //         0: {
+    //             items: 2
+    //         },
+    //         600: {
+    //             items: 3
+    //         },
+    //         850: {
+    //             items: 6
+    //         }
+    //     }
+    // });
+
+//     $('.owl-carousel.photo_carousel').owlCarousel({
+//         loop: true,
+//         margin: 0,
+//         nav: false,
+//         dots: false,
+//         items: 1,
+//         animateOut: 'fadeOut',
+//         animateIn: 'fadeIn',
+//         stagePadding:30,
+//         smartSpeed:450,
+//         autoplay: true,
+//         autoplayTimeout: 3000
+//     });
+// });
 $(document).ready(function () {
     $(".owl-carousel.photo_carousel").owlCarousel({
         loop: true,
         margin: 30,
-        items: 1
-    });
-});
-
-$(document).ready(function () {
-
-    $('.owl-carousel.photo_carousel').owlCarousel({
-        loop: true,
-        margin: 0,
-        nav: true,
+        nav: false,
+        dots: false,
+        items: 1,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        stagePadding:30,
+        smartSpeed:450,
+        autoplay: true,
+        autoplayTimeout: 3000,
         responsive: {
             0: {
                 items: 1
             },
+            400: {
+                items: 1
+            },
             600: {
+                items: 1
+            },
+            850: {
                 items: 1
             },
             1000: {
@@ -54,6 +89,9 @@ $(document).ready(function () {
         }
     });
 });
-
-
-
+// $(window).load(function() {
+//     var images = $('img');
+//     images.each(function(i) {
+//         $(this).width($(this).width() / 2);
+//     });
+// });
